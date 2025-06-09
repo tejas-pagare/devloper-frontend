c0bc28e6532a86d480a0380d32f1597b26424481
tejas-pagare <147067782+tejas-pagare@users.noreply.github.com>
2025-06-09 19:20:25 +0530

    constant file backend url update

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,4 +2,4 @@
 // export const BACKEND_BASE_URL = "/api"
 
 // dev
-export const BACKEND_BASE_URL = location.hostname ? "http://localhost:3000":"/api"
+export const BACKEND_BASE_URL = location.hostname==="localhost" ? "http://localhost:3000":"/api";
